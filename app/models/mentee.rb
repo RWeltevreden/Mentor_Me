f00d52a784994d6d51a3908807b2cc1c887d6b45
Anna Putt anna.putt@lewagon.orgexec zs <anna.putt@lewagon.org>
2024-03-08 12:02:54 +0100

    validation mentee

--- app/models/mentee.rb
+++ app/models/mentee.rb
@@ -1,7 +1,7 @@
 class Mentee < ApplicationRecord
   belongs_to :user
 
-  has_many :connections, dependent: :destroy
+  has_one :connection, dependent: :destroy
   validates :goal, presence: true
 
   def index
