cbcd56d810ce8d486598821f32b08ba9b07c17b4
Romee Weltevreden <romeeweltevreden@hotmail.com>
2024-03-13 11:55:15 +0100

    added ratings to leaving reviews

--- app/javascript/controllers/addreviews_controller.js
+++ app/javascript/controllers/addreviews_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["reviewmodal"]
+  static values = { num: Number }
 
   new(event) {
     event.preventDefault()
@@ -11,6 +12,31 @@ export default class extends Controller {
   close(event) {
     event.preventDefault()
     this.reviewmodalTarget.style.display = 'none'
+  }
+
+  fill(event) {
+    // Call the removeStars() method before applying new styling
+    // console.log(event.target);
+    this.removeStars();
+    const n = event.target.dataset.value;
+
+    // Apply new styling to stars
+    const stars = this.element.querySelectorAll(".star");
+
+    [...stars].forEach((star, index) => {
+      if (index <= n) {
+        // console.log(star.dataset.value);
+        star.classList.add("gold");
+      }
+    })
+  }
+
+  removeStars() {
+    // Remove pre-applied styling
+    const stars = this.element.querySelectorAll(".star");
+    stars.forEach((star) => {
+      star.classList.remove("gold");
+    });
   }
 
 }
